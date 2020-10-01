020a98389ae9374e75f36c35c4332ad153dfd1af
Finbar Berkon <finbar.berkon@unsw.edu.au>
2020-10-01 16:24:38 +1000

    week 3 tutorial

---
+++ tut03/q9_union.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+union _all {
+   int          ival;
+   char         cval;
+   char         sval[4];
+   float        fval;
+   unsigned int uval;
+};
+
+int main(void) {
+    union _all var;
+    var.uval = 0x00313233; // 0b00000000001100010011001000110011
+    printf("%x\n", var.uval);
+    printf("%d\n", var.ival);
+    printf("%c\n", var.cval);
+    printf("%s\n", var.sval);
+    printf("%f\n", var.fval);
+    printf("%e\n", var.fval);
+}
---
+++ tut03/six_middle_bits.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <assert.h>
+
+void print_bits(uint32_t w) {
+    int size = sizeof(uint32_t) * 8;
+    for (int i = 0; i < size; i++) {
+        uint32_t mask = 1u << (size - i - 1);
+        if (w & mask) {
+            printf("1");
+        } else {
+            printf("0");
+        }
+    }
+    printf("\n");
+}
+
+uint32_t six_middle_bits(uint32_t num) {
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2) {
+        return 1;
+    }
+
+    uint32_t num = strtol(argv[1], NULL, 10);
+    print_bits(num);
+    uint32_t res = six_middle_bits(num);
+    print_bits(res);
+}
